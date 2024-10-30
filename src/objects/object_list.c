53e0aca5c61c19179e9351152ec4025f252b6b83
Patrick Smith <psmith5644@gmail.com>
2024-10-29 18:20:49 -0700

    Refactor Object list to adopt responsiblity of setting the index of the object being added; add error codes and documentation

--- src/objects/object_list.c
+++ src/objects/object_list.c
@@ -1,25 +1,54 @@
 #include "object_list.h"
+#include "log.h"
+#include "errors.h"
 
+/**
+ ******************************************************************************
+ * @brief GameObjectList ctor, initializes bitmap that tracks used indices
+ ******************************************************************************
+ */
 void GameObjectList_ctor(GameObjectList * const me) {
     me->bitmap = 0U;
 }
 
-uint8_t GameObjectList_add(GameObjectList * const me, GameObject * const obj) {
+/**
+ ******************************************************************************
+ * @brief Adds a game object to the list of all game objects, used by the game 
+ *        engine.  Sets the object's index attribute.
+ * @param obj game object to add to the list
+ * @return error code
+ ******************************************************************************
+ */
+ERR_t GameObjectList_Add(GameObjectList * const me, GameObject * const obj) {
     uint8_t index = 0;
     while (index < MAX_OBJECTS) {
         if ((me->bitmap & (1 << index)) == 0) {
             me->objects[index] = obj;
             me->bitmap |= (1 << index);
-            return index;
+            obj->index = index;
+            return SUCCESS; 
         }
         index++;
     }
-    return -1; // TODO assert failed: caller must check is_Full() before add()
+    return ERR_OBJECTLIST_FULL;
 }
-void GameObjectList_remove(GameObjectList * const me, uint8_t index) {
+
+/**
+ ******************************************************************************
+ * @brief Removes object at provided @index from object list
+ * @param index of object to remove
+ ******************************************************************************
+ */
+void GameObjectList_Remove(GameObjectList * const me, uint8_t index) {
     me->bitmap &= ~(1 << index);
 }
 
+/**
+ ******************************************************************************
+ * @brief checks if game object list is full
+ * @return true of the object list is full, false otherwise
+ ******************************************************************************
+ */
 bool GameObjectList_isFull(GameObjectList const * const me) {
     return (~me->bitmap == 0);
 }
--- src/objects/object_list.h
+++ src/objects/object_list.h
@@ -12,9 +12,36 @@ typedef struct {
     uint32_t bitmap;
 } GameObjectList;
 
+/**
+ ******************************************************************************
+ * @brief GameObjectList ctor, initializes bitmap that tracks used indices
+ ******************************************************************************
+ */
 void GameObjectList_ctor(GameObjectList * const me);
-uint8_t GameObjectList_add(GameObjectList * const me, GameObject * const obj);
-void GameObjectList_remove(GameObjectList * const me, uint8_t index);
+
+/**
+ ******************************************************************************
+ * @brief Adds a game object to the list of all game objects, used by the game 
+ *        engine.  Sets the object's index attribute.
+ * @param obj game object to add to the list
+ * @return error code
+ ******************************************************************************
+ */
+ERR_t GameObjectList_Add(GameObjectList * const me, GameObject * const obj);
+
+/**
+ ******************************************************************************
+ * @brief Removes object at provided @index from object list
+ * @param index of object to remove
+ ******************************************************************************
+ */
+void GameObjectList_Remove(GameObjectList * const me, uint8_t index);
+/**
+ ******************************************************************************
+ * @brief checks if game object list is full
+ * @return true of the object list is full, false otherwise
+ ******************************************************************************
+ */
 bool GameObjectList_isFull(GameObjectList const * const me);
 
 #endif // __OBJECT_LIST_H__
